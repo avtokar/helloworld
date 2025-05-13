608028753226466574468f8c39eef822cc7dd405
avtokar <svigor1982@gmail.com>
2025-05-13 22:24:50 +0300

    Задание № 4

--- script.js
+++ script.js
@@ -14,3 +14,15 @@ for (let i = 1; i <= 5; i++) {
 for (let i = 7; i <= 22; i++) {
   console.log(i);
 }
+
+// Задание № 4
+const obj = {
+  "Коля": '200',
+  "Вася": '300',
+  "Петя": '400'
+};
+
+for (let key in obj) {
+  console.log(`${key} — зарплата ${obj[key]} долларов`);
+}
+
