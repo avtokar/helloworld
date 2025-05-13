b49c303beb7221c2871be7385ec921c588c59cf2
avtokar <svigor1982@gmail.com>
2025-05-13 22:37:47 +0300

    Задание № 2

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ function min(a, b) {
         return b;
     }
 }
+
+//задание 2
+function checkParity(number) {
+    if (number % 2 === 0) {
+        return 'Число четное';
+    }
+    else {
+        return 'Число нечетное';
+    }
+}
