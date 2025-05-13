7cbf907fff57e424eea75bfc52d2d38e86fa6220
avtokar <svigor1982@gmail.com>
2025-05-13 09:34:01 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,104 +1,89 @@
-//Задание № 1
-let password = 'Пароль!';
-let userInput = prompt('Введите пароль');
+//задание 1
+function min(a, b) {
 
-if (userInput === password) {
-  alert('Пароль введен верно');
-} else {
-  alert('Пароль введен неправильно');
-}
-
-//Задание № 2
-let c = 2;
-
-if (c > 0 && c < 10) {
-  console.log('Верно');
-} else {
-  console.log('Неверно');
-}
-
-//Задание № 3
-let d = 150;
-let e = 50;
-
-if (d > 100 || e > 100) {
-  console.log('Верно');
-} else {
-  console.log('Неверно');
-}
-
-//Задание № 4
-let a = '2';
-let b = '3';
-
-alert(+a + +b);
-
-//Задание № 5
-let monthNumber = 12;
-
-switch (monthNumber) {
-  case 1:
-  case 2:
-  case 12:
-    console.log('Зима');
-    break;
-  case 3:
-  case 4:
-  case 5:
-    console.log('Весна');
-    break;
-  case 6:
-  case 7:
-  case 8:
-    console.log('Лето');
-    break;
-  case 9:
-  case 10:
-  case 11:
-    console.log('Осень');
-    break;
-  default:
-    if (monthNumber > 13) {
-      console.log('Ошибка');
+    if (a < b) {
+        return a;
+    }
+    else {
+        return b;
     }
 }
 
-//Доп задание 1
-let input = Namber(prompt('Пожалуйста, введите любое число'));
+//задание 2
+function checkParity(number) {
+    if (number % 2 === 0) {
+        return 'Число четное';
+    }
+    else {
+        return 'Число нечетное';
+    }
+}
 
-if (isNaN(number)) {
-  alert('Введённое значение не является числом');
-} else {
-  if (number % 2 === 0) {
-    alert('Число чётное');
-  } else {
-    alert('Число нечётное');
+//задание 3
+function printSquare(number) {
+    console.log(number ** 2);
+}
+
+function getSquare(number) {
+    return number ** 2;
+}
+
+//задание 4
+function greetByAge() {
+    const age = prompt('Сколько вам лет?');
+
+    if (isNaN(age) || age < 0) {
+        alert('Вы ввели неправильное значение');
+    }
+    else if (age >= 0 && age <= 12) {
+        alert('Привет, друг!');
+    }
+    else {
+        alert('Добро пожаловать!');
+    }
+}
+
+//задание 5 
+function multiplyNumbers(num1, num2) {
+    if (isNaN(parseFloat(num1)) || isNaN(parseFloat(num2))) {
+        return 'Одно или оба значения не являются числом';
+    }
+    else {
+        return num1 * num2;
+    }
+}
+
+//задание 6 
+function cubeNumber() {
+  const input = prompt('Введите число');
+  const number = parseFloat(input);
+
+  if (isNaN(number)) {
+    return 'Переданный параметр не является числом';
+  } 
+  else {
+    const cube = number ** 3;
+    return `Число ${number} в кубе равняется ${cube}`;
   }
 }
 
-//доп задание 2
-let clientOS = 0; 
-
-if (clientOS === 0) {
-  console.log('Установите версию приложения для iOS по ссылке');
-} else if (clientOS === 1) {
-  console.log('Установите версию приложения для Android по ссылке');
-}
-
-//Доп задание 3
-let clientOS = 0; 
-let DeviceYear = 2015; 
-
-if (clientOS === 0) {
-  if (DeviceYear < 2015) {
-    console.log('Установите облегчённую версию приложения для iOS по ссылке');
-  } else {
-    console.log('Установите версию приложения для iOS по ссылке');
+//задание 7
+const circle1 = {
+  radius: 0,
+  getArea: function() {
+    return Math.PI * this.radius ** 2;
+  },
+  getPerimeter: function() {
+    return 2 * Math.PI * this.radius;
   }
-} else if (clientOS === 1) {
-  if (DeviceYear < 2015) {
-    console.log('Установите облегчённую версию приложения для Android по ссылке');
-  } else {
-    console.log('Установите версию приложения для Android по ссылке');
+};
+
+const circle2 = {
+  radius: 0,
+  getArea: function() {
+    return Math.PI * this.radius ** 2;
+  },
+  getPerimeter: function() {
+    return 2 * Math.PI * this.radius;
   }
-}
+};
