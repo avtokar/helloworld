0d0119a2439656d362ab67aa44dfe92afcce6bc7
avtokar <svigor1982@gmail.com>
2025-05-22 16:48:06 +0300

    Создание игры Простая арифметика

--- script.js
+++ script.js
@@ -23,3 +23,48 @@ function startGame() {
 
     guessNumberGame();
 }
+
+function startGameArithmetic() {
+    alert("Добро пожаловать в арифметическую игру!");
+    alert("Решите предложенные арифметические задачи.");
+
+    const operators = ['+', '-', '*', '/'];
+    const num1 = Math.floor(Math.random() * 20) + 1;
+    const num2 = Math.floor(Math.random() * 20) + 1;
+    const operator = operators[Math.floor(Math.random() * operators.length)];
+    const task = `${num1} ${operator} ${num2}`;
+
+    alert(`Задача: ${task}`);
+
+    const userAnswer = parseFloat(prompt("Введите ваш ответ: "));
+
+    let correctAnswer;
+
+    switch (operator) {
+        case '+':
+            correctAnswer = num1 + num2;
+            break;
+        case '-':
+            correctAnswer = num1 - num2;
+            break;
+        case '*':
+            correctAnswer = num1 * num2;
+            break;
+        case '/':
+            correctAnswer = num1 / num2;
+            break;
+    }
+
+    if (userAnswer === correctAnswer) {
+        alert("Верно!");
+    } else {
+        alert(`Ошибка. Правильный ответ: ${correctAnswer}`);
+    }
+
+    const playAgain = confirm("Хотите сыграть еще раз?");
+    if (playAgain) {
+        startGameArithmetic();
+    } else {
+        alert("Спасибо за игру! До свидания.");
+    }
+}
