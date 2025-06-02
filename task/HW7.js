1a14889565f06df2fa6e3e9e27d172f32aeb0de1
avtokar <svigor1982@gmail.com>
2025-06-02 13:26:27 +0300

    Задача № 3

--- task/HW7.js
+++ task/HW7.js
@@ -13,3 +13,15 @@ function filterByPrefix(arrayOfStrings, prefix) {
 const strings = ["Apple", "banana", "Apricot", "cherry", "Avocado"];
 const prefix = "a";
 filterByPrefix(strings, prefix); 
+
+//Задача № 3
+const number = 32.58884;
+
+const floorNumber = Math.floor(number);
+console.log(floorNumber); 
+
+const ceilNumber = Math.ceil(number);
+console.log(ceilNumber); // Выводит: 33
+
+const roundNumber = Math.round(number);
+console.log(roundNumber); 
