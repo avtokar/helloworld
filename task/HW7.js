c98d9a41242cc05f208b032d402bed787b18718c
avtokar <svigor1982@gmail.com>
2025-06-02 13:25:40 +0300

    Задача № 2

--- task/HW7.js
+++ task/HW7.js
@@ -3,4 +3,13 @@ const str = 'js';
 const upperCaseStr = str.toUpperCase();
 console.log(upperCaseStr);
 
+//Задача № 2
+function filterByPrefix(arrayOfStrings, prefix) {
+  const result = arrayOfStrings.filter(str => str.toLowerCase().startsWith(prefix.toLowerCase()));
+  console.log(result);
+  return result;
+}
 
+const strings = ["Apple", "banana", "Apricot", "cherry", "Avocado"];
+const prefix = "a";
+filterByPrefix(strings, prefix); 
