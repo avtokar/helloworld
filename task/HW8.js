2479761f959a857dfa8f4c500418bd5cf98a591c
avtokar <svigor1982@gmail.com>
2025-06-03 16:13:25 +0300

    Задание № 3

--- task/HW8.js
+++ task/HW8.js
@@ -8,3 +8,18 @@ const people = [
 
 console.log(people.sort((a, b) => a.age - b.age));
 
+//Задание № 2
+
+//Задание № 3
+function printCurrentDate() {
+  const intervalId = setInterval(() => {
+    console.log(new Date());
+  }, 3000); // 3000 миллисекунд = 3 секунды
+
+  setTimeout(() => {
+    clearInterval(intervalId);
+    console.log('30 секунд прошло');
+  }, 30000); // 30000 миллисекунд = 30 секунд
+}
+
+printCurrentDate();
