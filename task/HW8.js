7bbf6322e7e86687d3830f90784c7d172bf35b21
avtokar <svigor1982@gmail.com>
2025-06-03 16:15:08 +0300

    Задание 4

--- task/HW8.js
+++ task/HW8.js
@@ -25,3 +25,10 @@ function printCurrentDate() {
 printCurrentDate();
 
 //Задание № 4
+function delayForSecond(callback) {
+  setTimeout(callback, 1000); 
+}
+
+delayForSecond(function () {
+  console.log('Привет, Глеб!');
+});
