3732acb1d4e8b3f6a2b11613f0187f9d7fd69910
avtokar <svigor1982@gmail.com>
2025-06-03 16:22:50 +0300

    Заданире 2

--- task/HW8.js
+++ task/HW8.js
@@ -10,6 +10,35 @@ console.log(people.sort((a, b) => a.age - b.age));
 
 //Задание № 2
 
+function isPositive(num) {
+  return num > 0;
+}
+
+function isMale(person) {
+  return person.gender === 'male';
+}
+
+function filter(arr, ruleFunction) {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (ruleFunction(arr[i])) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
+
+console.log(filter([3, -4, 1, 9], isPositive));
+
+const people = [
+  {name: 'Глеб', gender: 'male'},
+  {name: 'Анна', gender: 'female'},
+  {name: 'Олег', gender: 'male'},
+  {name: 'Оксана', gender: 'female'}
+];
+
+console.log(filter(people, isMale));
+
 //Задание № 3
 function printCurrentDate() {
   const intervalId = setInterval(() => {
