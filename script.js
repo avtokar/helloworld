01bc1e9f83fff64a2e4b313c0ed78310425e91bc
avtokar <svigor1982@gmail.com>
2025-06-16 12:09:11 +0300

    Создание игры "Генератор случайных цветов"

--- script.js
+++ script.js
@@ -172,3 +172,9 @@ function startTsuefaGame() {
   }
 
 }
+
+//Игра Генератор случайных цветов
+function changeBackgroundColor() {
+  const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  document.getElementById('colorGame').style.backgroundColor = randomColor;
+}
